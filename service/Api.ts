bafa2ac4492694b7eef7c0e17dc19361bbc1d537
supriadi <supriadiadi1001@gmail.com>
2024-08-30 08:34:17 +0700

    update

--- service/Api.ts
+++ service/Api.ts
@@ -1,37 +1,46 @@
-//service api
-
 import { ApiUrl } from "../config/config";
 
 class Api {
   public url: string = "";
   public auth: boolean = false;
-  public type: "form" | "json" = "json";
+  public type: "form" | "json" | "multipart" = "json"; // Tambahkan tipe "multipart" untuk FormData
   public token: string = "";
   public header: any = {};
   public body: any = {};
 
   public call = async () => {
     const url = ApiUrl + this.url;
-    const headers = {
+    const headers: any = {
       ...this.header,
-      "Content-Type":
-        this.type === "json"
-          ? "application/json"
-          : "application/x-www-form-urlencoded",
     };
 
+    // Hanya set Content-Type jika bukan multipart/form-data
+    if (this.type === "json") {
+      headers["Content-Type"] = "application/json";
+    } else if (this.type === "form") {
+      headers["Content-Type"] = "application/x-www-form-urlencoded";
+    }
+
+    // Tambahkan Authorization jika dibutuhkan
     if (this.auth && this.token) {
       headers["Authorization"] = "Bearer " + this.token;
       headers["Accept"] = "application/json";
     }
 
+    // Set body sesuai dengan tipe request
+    let body: BodyInit;
+    if (this.type === "json") {
+      body = JSON.stringify(this.body);
+    } else if (this.type === "form") {
+      body = new URLSearchParams(this.body).toString();
+    } else {
+      body = this.body; // Jika tipe multipart, body sudah berbentuk FormData
+    }
+
     const options: RequestInit = {
       method: "POST",
       headers: headers,
-      body:
-        this.type === "json"
-          ? JSON.stringify(this.body)
-          : new URLSearchParams(this.body).toString(),
+      body: body,
     };
 
     try {
@@ -40,6 +49,7 @@ class Api {
 
       return data;
     } catch (error) {
+      console.error("API call error:", error);
       return {
         meta: {
           code: 400,
---
+++ src/components/dashboard/modal/AddPhotoProfile.tsx
@@ -0,0 +1,113 @@
+import React, { useState } from "react";
+import Image from "next/image";
+import { Button } from "@material-tailwind/react";
+import { toast, ToastContainer } from "react-toastify";
+import { usePhotoProfile } from "../../../../hooks/usePhotoProfile";
+import { useSession } from "next-auth/react";
+import { ApiUrl } from "../../../../config/config";
+
+interface AddPhotoProfileProps {
+  token: string;
+  userId: any;
+
+  id: number | undefined;
+  onClose: () => void;
+}
+
+const AddPhotoProfile: React.FC<AddPhotoProfileProps> = ({
+  token,
+  userId,
+  id,
+  onClose,
+}) => {
+  const { uploadPhoto, loading } = usePhotoProfile(token);
+  const { data: session } = useSession() as any;
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+  const [previewUrl, setPreviewUrl] = useState<string | null>(
+    session?.user?.Murid?.photo ? ApiUrl + "/" + session.user.Murid.photo : null
+  );
+
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files[0]) {
+      const file = e.target.files[0];
+      setSelectedFile(file);
+      setPreviewUrl(URL.createObjectURL(file)); // Generate preview URL for the selected file
+    }
+  };
+
+  const handleUpload = async () => {
+    if (selectedFile) {
+      const result = await uploadPhoto(userId, id ?? 0, selectedFile);
+      if (result.success) {
+        onClose(); // Close the modal only on success
+      }
+    } else {
+      toast.error("Pilih foto terlebih dahulu.", { autoClose: 3000 });
+    }
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center p-4 ">
+      <h2 className="text-2xl font-bold mb-5 text-center text-black">
+        Lengkapi Profile Anda
+      </h2>
+
+      <div className="relative w-32 h-32 mb-4 ">
+        <div className="relative rounded-full w-full h-full overflow-hidden bg-gray-500">
+          {previewUrl && (
+            <Image
+              src={previewUrl}
+              alt="Preview"
+              layout="fill"
+              objectFit="cover"
+              className="rounded-full"
+            />
+          )}
+        </div>
+
+        <div className=" w-full flex flex-col justify-start items-center">
+          <input
+            type="file"
+            id="file-upload"
+            onChange={handleFileChange}
+            className="hidden"
+          />
+          <label htmlFor="file-upload">
+            <p
+              className="absolute bottom-2 right-5 transform translate-y-1/2 translate-x-1/2 rounded-full bg-green-500 w-10 h-10 text-white text-2xl flex items-center justify-center"
+              style={{ zIndex: 10 }}
+            >
+              +
+            </p>
+          </label>
+          <p className="mt-4 text-gray-600 w-40 text-center">
+            Upload your photo
+          </p>
+          <div className="mt-4 flex justify-end space-x-2">
+            <Button
+              color="green"
+              onClick={handleUpload}
+              disabled={loading || !selectedFile}
+            >
+              {loading ? "Uploading..." : "Upload"}
+            </Button>
+          </div>
+        </div>
+      </div>
+
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar
+        newestOnTop
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+    </div>
+  );
+};
+
+export default AddPhotoProfile;
